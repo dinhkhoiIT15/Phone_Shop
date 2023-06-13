e75c79d0b02448e57bfb2c02264db265bf710c53
Pham Truong <pvntruongit03@gmail.com>
2023-06-13 09:17:50 +0700

    add phone

--- src/Form/AddPhoneType.php
+++ src/Form/AddPhoneType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Phone;
+use App\Entity\Product;
+use App\Entity\Supplier;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -19,6 +22,29 @@ class AddPhoneType extends AbstractType
             ->add('phone_name',TextType::class,['label_attr' => ['class' => 'form-label'], 'attr' => ['class' => 'form-control']])
             ->add('price',TextType::class,['label_attr' => ['class' => 'form-label'], 'attr' => ['class' => 'form-control']])
             ->add('date',DateType::class,['widget' => 'choice',])
+            ->add('product',EntityType::class, [
+                "class"=>Product::class,
+                "query_builder"=>function(EntityRepository $er)
+                {
+                    return $er->createQueryBuilder('p')
+                        ->orderBy('p.pro_name');
+                },
+                "choice_label"=>'pro_name',
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('suppliers',EntityType::class, [
+                "class"=>Supplier::class,
+                "query_builder"=>function(EntityRepository $er)
+                {
+                    return $er->createQueryBuilder('s')
+                        ->orderBy('s.sup_name');
+                },
+                "choice_label"=>'sup_name',
+                "multiple"=>true,
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control']
+            ])
             ->add('submit', SubmitType::class, ['attr' => ['class' => 'btn btn-success']]);
         ;
     }
